3dea1231bf46f5b67bf7e314d69021c1358c7ca8
Emily Mazo <emazo@caltech.edu>
2016-09-27 21:07:43 -0700

    adding plotting of sentiment by timeline with interruptions

--- analysis.py
+++ analysis.py
@@ -6,20 +6,28 @@ from nltk.tree import Tree
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import operator
 import sys  
+import matplotlib.pyplot as plt
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
-#import matplotlib.pyplot as plt
 
 def AttributeLines():
+    ''' This method separates the debate transcript raw text into three files by the speaker of the line
+    (Hillary Clinton, Donald Trump, or Lester Holt (Moderator) and the "line number," which is the count 
+        of the statement in the NYT transcript.
+    In this method I also keep track of when speakers are interrupted (lovingly annotated by the NYT as '...')
+    and who was interrupted. '''
+
     f = codecs.open('debate_transcript.txt', 'r')
     lines = f.readlines()
     h = codecs.open('hillary_lines.txt', 'w')
     t = codecs.open('trump_lines.txt', 'w')
     m = codecs.open('moderator_lines.txt', 'w')
+    i = codecs.open('interruption_counter.txt', 'w')
     count = 0
     for line in lines:
+        count += 1
         line = line.strip()
         line = line.split(' ')
         if line == ['']:
@@ -30,45 +38,58 @@ def AttributeLines():
                 if word == 'CLINTON:':
                     continue
                 sentence = sentence + ' ' + word
-                if word == 'Trump':
-                    count += 1
-            h.write(sentence)
+            h.write(str(count) + '\t' + sentence)
             h.write('\n')
+            if '...' in line:
+                i.write(str(count) + '\t' + 'h')
+                i.write('\n')
         elif 'TRUMP:' in line:
             sentence = ''
             for word in line:
                 if word == 'TRUMP:':
                     continue
                 sentence = sentence + ' ' + word
-            t.write(sentence)
+            t.write(str(count) + '\t' + sentence)
             t.write('\n')
+            if '...' in line:
+                i.write(str(count) + '\t' + 't')
+                i.write('\n')
         elif 'HOLT:' in line:
             sentence = ''
             for word in line:
                 if word == 'HOLT:':
                     continue
                 sentence = sentence + ' ' + word
-            m.write(sentence)
+            m.write(str(count) + '\t' + sentence)
             m.write('\n')
-    print count
+            if '...' in line:
+                i.write(str(count) + '\t' + 'm')
+                i.write('\n')
+
+        
 
 def AnalyzeLines(filename):
     h = codecs.open(filename, 'r')
     lines = h.readlines()
     named_entities = {}
-    sentiment_nums = np.zeros(len(lines))    
+    sentiment_nums = np.zeros(len(lines))
+    sentiment_by_line_count = []  
     count = 0
     for line in lines:
-        sentiment_nums[count] = FindSentiment(line)
+        line = line.split('\t')
+        line_num = line[0]
+        sentence = line[1]
+        sentiment_nums[count] = FindSentiment(sentence)
+        sentiment_by_line_count.append([line_num, sentiment_nums[count]])
         count += 1
-        new_entities = FindNamedEntities(line)
+        new_entities = FindNamedEntities(sentence)
         if new_entities != []:
             for entity in new_entities:
                 if entity not in named_entities.keys():
                     named_entities[entity] = 1
                 named_entities[entity] += 1
     sentiment_avg = np.mean(sentiment_nums)  
-    return (named_entities, sentiment_avg)
+    return (named_entities, sentiment_avg, sentiment_by_line_count)
 
 
 def FindNamedEntities(line):
@@ -88,19 +109,65 @@ def FindNamedEntities(line):
                     continue
     return continuous_chunk
 
+
 def FindSentiment(line):
     sid = SentimentIntensityAnalyzer()
     ss = sid.polarity_scores(line)
     return ss["compound"]
 
+
+def DrawSentGraphs(sents1, sents2):
+    '''This method plots the compound sentiment score of each statement of each candidate, 
+    over the time point in the debate which is measured by the number of statements 
+    (as annotated by the NYT in their debate transcript) that have been spoken by all
+    members of the debate (candidates and moderator).
+    Also the NYT helpfully annotated all interruptions with elipses ('...'), 
+    so I kept track of that too. Those are plotted as vertical black lines by time point.'''
+    # Keep track of interruptions (line counts)
+    i = codecs.open('interruption_counter.txt', 'r')
+    interruption_line_numbers = i.readlines()
+    # Separate line number and sentiment score (compound) data into X and Y coordinate data point lists
+    sents1_x = []
+    sents2_x = []
+    sents1_y = []
+    sents2_y = []
+    for i in range(len(sents1)):
+        sents1_x.append(sents1[i][0])
+        sents1_y.append(sents1[i][1])
+    for j in range(len(sents2)):
+        sents2_x.append(sents2[j][0])
+        sents2_y.append(sents2[j][1])
+    # Plot data
+    plt.ylim(-2.0, 2.0)
+    plt.plot(sents1_x, sents1_y, 'blue')
+    plt.plot(sents2_x, sents2_y, 'red')
+    for interruption in interruption_line_numbers:
+        interruption = interruption.strip()
+        interruption = interruption.split('\t')
+        interruption[0] = int(interruption[0])
+        if interruption[1] == 'h':
+            plt.plot((interruption[0], interruption[0]), (-2.0, 2.0), 'blue')
+        elif interruption[1] == 't':
+            plt.plot((interruption[0], interruption[0]), (-2.0, 2.0), 'red')
+        elif interruption[1] == 'm':
+            plt.plot((interruption[0], interruption[0]), (-2.0, 2.0), 'black')
+    # Add metadata to plot
+    plt.xlabel("Time Point in Debate")
+    plt.ylabel("Compound Sentiment Score")
+    plt.title("Sentiment Score for Candidate Statements Over the Debate Timeline")
+    # Show plot
+    plt.show()
+
+
 if __name__ == "__main__": 
     # Scrape debate transcript for lines from the three players
     AttributeLines()
-    trump_NE, trump_sent = AnalyzeLines('trump_lines.txt')
-    hillary_NE, hillary_sent = AnalyzeLines('hillary_lines.txt')
+    trump_NE, trump_sent, trump_sent_nums = AnalyzeLines('trump_lines.txt')
+    hillary_NE, hillary_sent, hillary_sent_nums = AnalyzeLines('hillary_lines.txt')
     print "Hillary sent: " + str(hillary_sent)
     print "Trump sent: " + str(trump_sent)
     hillary_sorted_NE = sorted(hillary_NE.items(), key=operator.itemgetter(1)) 
     trump_sorted_NE = sorted(trump_NE.items(), key=operator.itemgetter(1)) 
     print hillary_sorted_NE[-1]
     print trump_sorted_NE[-1]
+    DrawSentGraphs(hillary_sent_nums, trump_sent_nums)
